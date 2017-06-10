478ba22f70e29164643e2e21bcf06cf84dc7f6bf
mitchell <mitchellwarr@gmail.com>
2017-06-10 16:15:07 +1200

    added sql code

---
+++ sql/README.txt
@@ -0,0 +1,10 @@
+IN PSQL
+
+PSQL> \i 'C:\\path\\create db.sql'
+PSQL> \c nz_network
+PSQL> \i 'C:\\path\\insert db prt1.sql'
+PSQL> \i 'C:\\path\\insert db prt2.sql'
+PSQL> \i 'C:\\path\\insert db prt3.sql'
+
+\\ instead of \
+' instead of "
---
+++ sql/create db.sql
@@ -0,0 +1,12 @@
+-- Database: nz_network
+
+DROP DATABASE nz_network;
+
+CREATE DATABASE nz_network
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'English_New Zealand.1252'
+    LC_CTYPE = 'English_New Zealand.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
